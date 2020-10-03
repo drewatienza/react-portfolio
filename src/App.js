a2a3b4672882010c5e0daf24940a30b61b148940
drewatienza <atienza.drew@gmail.com>
2020-10-03 13:26:33 -0700

    created navigation component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Navigation from "./components/navigation/navigation.component";
 
 import "./App.css";
 
-function App() {
-  return <div></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route exact path="/">
+            This is the home page
+          </Route>
+          <Route exact path="/about">
+            This is the about page
+          </Route>
+          <Route exact path="/portfolio">
+            This is the portfolio page
+          </Route>
+          <Route exact path="/contact">
+            This is the contact page
+          </Route>
+          <Route exact path="/resume">
+            This is the resume page
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => (
+  <div className="nav-bar">
+    <Link className="logo-container" to="/">
+      Drew Atienza
+      {/* <Logo className='logo' /> */}
+    </Link>
+    <div className="options">
+      <Link className="option" to="/about">
+        ABOUT
+      </Link>
+      <Link className="option" to="/portfolio">
+        PORTFOLIO
+      </Link>
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
+      <Link className="option" to="/resume">
+        RESUME
+      </Link>
+    </div>
+  </div>
+);
+
+export default Navigation;
