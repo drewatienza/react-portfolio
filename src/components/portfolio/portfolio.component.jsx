666f11f088efbc1ad1f22ea48ee2657b91992097
drewatienza <atienza.drew@gmail.com>
2020-12-23 13:29:09 -0800

    updated portolio with 2 more projects

--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { DiFirebase, DiGithubBadge, DiHeroku, DiJavascript1, DiReact, DiSass } from 'react-icons/di';
+import { DiCss3, DiGithubBadge, DiHeroku, DiJavascript1, DiReact, DiSass } from 'react-icons/di';
 import { BiWindowOpen } from 'react-icons/bi';
+import { SiFirebase, SiGithub, SiNetlify } from 'react-icons/si'
 
 import Clothing from '../../assets/clothing.png';
 import BoxOffice from '../../assets/box-office.png';
 import GitHub from '../../assets/github-finder.png';
+import Footer from "../footer/footer.component";
 
 import './portfolio.styles.scss';
 
@@ -17,28 +19,15 @@ const Portfolio = () => {
         </div>
         <div className="port-description">
           <h3>Maricel's Clothing</h3>
-          {/* <h4>Group Project | One Week</h4> */}
           <div className="icon-container">
-            <div>
-              <DiReact />
-            </div>
-              <DiSass />
-            <div>
-              <DiJavascript1 />
-            </div>
-            <div>
-              <DiFirebase />
-            </div>
-            <div>
-              <DiHeroku />
-            </div>
+            <DiReact />
+            <DiSass />
+            <DiJavascript1 />
+            <SiFirebase />
+            <DiHeroku />
           </div>
           <p>
-            Maricel's Clothing is a boutique clothing store website.
-          </p>
-
-          <p>
-            This project was was built with React and Firebase.  The concept for Spoonder was an idea of mine after a peer suggested we build dating app for our final project. We based Spoonder on Tinder (incase you hadn't already guessed). The majority of the backend was built as a 3 using VS Code Live Share. I then built out a number of the frontend components and did all design work and Sass.
+            This is an ecommerce website for a boutique clothing store.  It was built with React and Firebase.
           </p>
           <div className="portfolio-btn">
             <a href="https://maricel-boutique.herokuapp.com/" target="_blank" rel='noopener noreferrer'><BiWindowOpen /> Live Site</a>
@@ -46,6 +35,53 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
+
+      {/* Project 2 */}
+      <div className="project-container">
+        <div className="image">
+          <img src={GitHub} alt='github finder screenshot' />
+        </div>
+        <div className="port-description">
+          <h3>GitHub Finder</h3>
+          <div className="icon-container">
+            <DiReact />
+            <DiJavascript1 />
+            <DiCss3 />
+            <SiNetlify />
+          </div>
+          <p>
+            GitHub Finder is a web app that lets users search for GitHub accounts using the GitHub API.
+          </p>
+          <div className="portfolio-btn">
+            <a href="https://drewatienza-github-finder.netlify.app/" target="_blank" rel='noopener noreferrer'><BiWindowOpen /> Live Site</a>
+            <a href="https://github.com/drewatienza/github_finder"><DiGithubBadge /> GitHub</a>
+          </div>
+        </div>
+      </div>
+
+      {/* Project 3 */}
+      <div className="project-container">
+        <div className="image">
+          <img src={BoxOffice} alt='box office screenshot' />
+        </div>
+        <div className="port-description">
+          <h3>Box Office</h3>
+          <div className="icon-container">
+            <DiReact />
+            <DiJavascript1 />
+            <DiCss3 />
+            <SiGithub />
+          </div>
+          <p>
+            This is a React project that lets users search Movie titles or search for actors and actresses.
+          </p>
+          <div className="portfolio-btn">
+            <a href="https://drewatienza.github.io/box-office/" target="_blank" rel='noopener noreferrer'><BiWindowOpen /> Live Site</a>
+            <a href="https://github.com/drewatienza/box-office"><DiGithubBadge /> GitHub</a>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
